415779226c796161cb4ab4331a49317c287c0146
Josh Olds <joshua.olds@outlook.com>
2016-11-05 17:21:33 -0600

    Fixed request file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 Author: Joshua Olds
 */
 var interval = 15;
-var address = 'https://boiseweather.herokuapp.com/'
+var address = 'https://boiseweather.herokuapp.com/api/weather'
 
 var fs = require('fs');
 var request = require('request');
@@ -21,11 +21,28 @@ function postData(){
   if(!data.tempF){return console.log("Error reading data file!!!")}
   if(data.init){return console.log("Python hasn't written data yet!")}
   var now = Date.now();
+  var weather = {dateStamp: now, reading: {tempF: data.tempF, tempC: data.tempC, rh: data.rh}}
+
+  var options = {
+    method: 'post',
+    body: weather,
+    json: true,
+    url: address
+  }
   console.log(`Posting data!`)
-  request.post(address, {dateStamp: now, reading: {tempF: data.tempF, tempC: data.tempC, rh: data.rh}})
+  request(options, function (err, res, body) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    var headers = res.headers
+    var statusCode = res.statusCode
+    console.log("Successful Post!")
+  })
 }
 
 postData();
+console.log(`Waiting till next post for: ${interval} minutes...`)
 setInterval(postData, interval * 60 * 1000);
 
 
