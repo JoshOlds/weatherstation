e26c0ad75755c09533491860404021c7617da89f
Joshua Olds <joshua.olds@outlook.com>
2016-11-07 10:34:22 -0700

    Added lat/lon

--- AdafruitDHT.py
+++ AdafruitDHT.py
@@ -55,7 +55,7 @@ while True:
     # If this happens try again!
     if rh is not None and tempC is not None:
         print "Python writing to file!"
-        f.write('{{"tempF": {0:0.1f}, "tempC": {1:0.1f}, "rh":{2:0.1f}}}'.format(tempF, tempC, rh))
+        f.write('{{"tempF": {0:0.1f}, "tempC": {1:0.1f}, "rh":{2:0.1f}, location: {lat: 43.60007449999999, lon: -116.23737749999998}}}'.format(tempF, tempC, rh))
     else:
         print('Failed to get reading. Try again!')
     print "Python closing file."
